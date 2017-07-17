ac59faa8ebfa68bae251ab9bdc3a315e9617fbc0
Kate Byars <katebyars5@gmail.com>
2017-07-17 16:03:58 -0700

    add more rows of stars and animate them

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
 ///Front End Logic///
+  // $("div").hover(function(){
+  //   $("span").addClass(".two");
+  //   // $(this).fadeIn("slow");
+  //   // $(this).fadeOut("slow");
+  //   // $(this).fadeIn("slow");
+  //   // $(this).fadeOut("fast");
+  //   // $(this).hide();
+  //   // $(this).show();
+  //   // $(this).fadeIn("slow");
+  // });
+
   $(".star").hover(function(){
-    $(this).fadeOut("fast");
     $(this).fadeIn("slow");
     $(this).fadeOut("slow");
     $(this).fadeIn("slow");
+    $(this).fadeOut("fast");
     $(this).hide();
     $(this).show();
     $(this).fadeIn("slow");
   });
+
 });
 
 ///Back End Logic///
